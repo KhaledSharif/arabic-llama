2f4e2885cd2651061bf0ec42a2982798a3ed438b
Khaled Sharif <kldsrf@gmail.com>
2024-05-15 14:42:48 -0700

    add jsonl

--- generate.py
+++ generate.py
@@ -1,88 +1,119 @@
 from datasets import load_dataset
+from datetime import datetime
+from json import dumps
 import ollama
 
 ollama_options = ollama.Options(
-    num_predict=1_000,
     seed=42,
-    temperature=0.5,
+    temperature=0.0,
     num_ctx=2_000,
+    num_predict=1_000,
 )
 
 model_name = "mistral:7b-instruct-v0.2-q6_K"
 
+dataset_n = 1_000
+
 dataset_name = "qiaojin/PubMedQA"
-dataset = load_dataset(dataset_name, "pqa_artificial", split="all")
-dataset = dataset.shuffle().select(range(10))
+dataset = load_dataset(dataset_name, "pqa_unlabeled", split="all")
+dataset = dataset.shuffle().select(range(dataset_n))
 
-for i in range(10):
-    question, answer = dataset["question"][i].strip(), dataset["long_answer"][i].strip()
-    context = "\n".join(dataset["context"][i]["contexts"])
+dt_now = datetime.now().strftime("%Y%m%d%H%M")
 
-    print("=" * 20, "Q", i + 1, "=" * 20, "\n")
-    print(context, "\n---\n")
-    print(
-        f"❓-- Question --", question, "📖 -- Original Answer --", answer, sep="\n\n", end="\n\n\n"
-    )
+with open(f"./results/{dt_now}.jsonl", "a") as output_json_lines:
+    for i in range(dataset_n):
+        question, answer = (
+            dataset["question"][i].strip(),
+            dataset["long_answer"][i].strip(),
+        )
+        context = "\n".join(dataset["context"][i]["contexts"]).strip()
 
-    answer_without_hint = ""
-    answer_with_hint = ""
+        print("=" * 20, "Q", i + 1, "=" * 20, "\n")
+        print(context, "\n---\n")
+        print(
+            f"❓-- Question --",
+            question,
+            "📖 -- Original Answer --",
+            answer,
+            sep="\n\n",
+            end="\n\n\n",
+        )
 
-    # ========================
+        answer_without_hint = ""
+        answer_with_hint = ""
 
-    prompt_no_hint = f"{context} Question: {question} Answer:"
+        # ========================
 
-    print("🕯️ -- LLM Answer w/out hint --")
-    stream = ollama.chat(
-        model=model_name,
-        messages=[
-            {"role": "user", "content": prompt_no_hint }
-        ],
-        stream=True,
-        options=ollama_options,
-    )
+        prompt_no_hint = f"{context} Question: {question} Answer:"
 
-    for chunk in stream:
-        c = chunk["message"]["content"]
-        answer_without_hint += c
-        print(c, end="", flush=True)
+        print("🕯️ -- LLM Answer w/out hint --")
+        stream = ollama.chat(
+            model=model_name,
+            messages=[{"role": "user", "content": prompt_no_hint}],
+            stream=True,
+            options=ollama_options,
+        )
 
-    print("\n\n\n")
+        for chunk in stream:
+            c = chunk["message"]["content"]
+            answer_without_hint += c
+            print(c, end="", flush=True)
 
-    # ========================
+        print("\n\n\n")
 
-    prompt_hint = f"{context} Question: {question} (Hint: {answer}) Answer:"
+        # ========================
 
-    print("💡 -- LLM Answer with hint --")
-    stream = ollama.chat(
-        model=model_name,
-        messages=[{"role": "user", "content": prompt_hint}],
-        stream=True,
-        options=ollama_options,
-    )
+        prompt_hint = f"{context} Question: {question} (Hint: {answer}) Answer:"
 
-    for chunk in stream:
-        c = chunk["message"]["content"]
-        answer_with_hint += c
-        print(c, end="", flush=True)
+        print("💡 -- LLM Answer with hint --")
+        stream = ollama.chat(
+            model=model_name,
+            messages=[{"role": "user", "content": prompt_hint}],
+            stream=True,
+            options=ollama_options,
+        )
 
-    print("\n")
+        for chunk in stream:
+            c = chunk["message"]["content"]
+            answer_with_hint += c
+            print(c, end="", flush=True)
 
-    stream = ollama.chat(
-        model=model_name,
-        messages=[
-            {"role": "user", "content": prompt_hint},
-            {"role": "assistant", "content": answer_with_hint},
-            {"role": "user", "content": "Explain Your Answer"},
-        ],
-        stream=True,
-        options=ollama_options,
-    )
+        print("\n")
 
-    for chunk in stream:
-        print(chunk["message"]["content"], end="", flush=True)
-        
-    print("\n\n")
+        answer_with_hint += "\n"
 
-    # ========================
+        stream = ollama.chat(
+            model=model_name,
+            messages=[
+                {"role": "user", "content": prompt_hint},
+                {"role": "assistant", "content": answer_with_hint},
+                {"role": "user", "content": "Explain Your Answer"},
+            ],
+            stream=True,
+            options=ollama_options,
+        )
 
-    print("=" * 50, "\n")
+        for chunk in stream:
+            c = chunk["message"]["content"]
+            answer_with_hint += c
+            print(c, end="", flush=True)
+
+        print("\n\n")
+
+        # ========================
+
+        output_json_lines.write(
+            dumps(
+                {
+                    "context": context,
+                    "question": question,
+                    "answer_original": answer,
+                    "answer_rejected": answer_without_hint,
+                    "answer_chosen": answer_with_hint,
+                }
+            )
+            + "\n"
+        )
+        output_json_lines.flush()
+
+        print("=" * 50, "\n")
