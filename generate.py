2743d84e28b6f355a3260c2e6fcadde3db2e9f2e
Khaled Sharif <kldsrf@gmail.com>
2024-05-15 02:14:26 -0700

    cleanup

--- generate.py
+++ generate.py
@@ -1,24 +1,5 @@
 from datasets import load_dataset
 import dspy
-from duckduckgo_search import DDGS
-
-# --------------------------------------------------------------
-#
-#   FREE-FORM ANSWER
-#   https://huggingface.co/datasets/medalpaca/medical_meadow_medical_flashcards
-#   https://huggingface.co/datasets/medalpaca/medical_meadow_wikidoc
-#
-#   MULTIPLE CHOICE
-#   https://huggingface.co/datasets/medalpaca/medical_meadow_medqa
-#
-# --------------------------------------------------------------
-
-
-def duck_search(query: str) -> str:
-    results = DDGS().text(query, max_results=10)
-    results = [x["title"] + "\n" + x["body"] for x in results]
-    return "\n\n".join(results)
-
 
 lm_provider = dspy.OllamaLocal(
     model="dolphin-mistral:7b-v2.8-q3_K_M",
@@ -29,23 +10,8 @@ lm_provider = dspy.OllamaLocal(
 
 dspy.settings.configure(lm=lm_provider)
 
-
-class QuestionAnswer(dspy.Signature):
-    """Use the context to form an answer to the question and explain your answer"""
-
-    question = dspy.InputField(desc="The question we are trying to answer")
-    context = dspy.InputField(
-        desc="List of potentially related information to the question"
-    )
-    answer = dspy.OutputField(
-        desc="""
-        Answer to the question using context and internal knowledge
-        """
-    )
-
-
-dspy_qa = dspy.ChainOfThought(QuestionAnswer)
-dspy_hint = dspy.ChainOfThoughtWithHint(QuestionAnswer)
+dspy_qa = dspy.ChainOfThought("question->answer")
+dspy_hint = dspy.ChainOfThought("question,hint->answer")
 
 dataset_name = "medalpaca/medical_meadow_medical_flashcards"
 dataset = load_dataset(dataset_name, split="all")
@@ -62,18 +28,11 @@ for i in range(10):
         f"Question {i+1}:", question, "Original Answer:", answer, sep="\n\n", end="\n\n"
     )
 
-    search_results = duck_search(query=question)
-    # print("-" * 20)
-    # print(search_results)
-    # print("-" * 20, "\n")
-
-    dspy_answer = dspy_qa(question=question, context=search_results).answer
+    dspy_answer = dspy_qa(question=question).answer
     print("LLM w/out hint:")
     print(dspy_answer, "\n")
 
-    dspy_answer_with_hint = dspy_hint(
-        question=question, context=search_results, hint=answer
-    ).answer
+    dspy_answer_with_hint = dspy_hint(question=question, hint=answer).answer
     print("LLM with hint:")
     print(dspy_answer_with_hint, "\n")
 
